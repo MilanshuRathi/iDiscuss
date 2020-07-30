436aec09406915c9a546b9d3157ca71fa69e326b
MilanshuRathi <milanshu2000@gmail.com>
2020-07-31 01:47:35 +0530

    changed port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const app=express();
 const http=require('http').createServer(app);
+const port=process.env.PORT||3000;
 app.use(express.static(`${__dirname}/public`));
 app.get('/',(request,response)=>{
     response.sendFile(`${__dirname}/chat.html`);    
